31be40da0fadab0965b04482fa4b9dd8db4b5bcb
王文杰 <wangwenjie@wangwenjiedeMacBook-Pro.local>
2018-08-05 16:55:26 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -14,38 +14,38 @@ gulp.task('scripts', function() {
 		  transform: [stringify([".html"])]
 		}))
         .pipe(uglify())
-        .pipe(gulp.dest('./build/js/'));
+        .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('css', function() {
   gulp.src('./src/css/**')
-    .pipe(gulp.dest('./build/css/'));
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/**', ['build']);
+    gulp.watch('src/**', ['dist']);
 });
 
 gulp.task('clean', function (cb) {
-    rimraf('./build', cb);
+    rimraf('./dist', cb);
 });
 
 gulp.task("url", function(){
   var options = {
     url: "http://localhost:3000"
   };
-  gulp.src("./build/index.html")
+  gulp.src("./dist/index.html")
   .pipe(open("", options));
 });
 
-gulp.task('serve', serve('build'));
+gulp.task('serve', serve('dist'));
 
 gulp.task('minify-html', function() {
     var opts = {comments:true,spare:true};
 
   gulp.src('./src/*.html')
     .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./build/'));
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('build', ['clean', 'scripts', 'css','minify-html']);
